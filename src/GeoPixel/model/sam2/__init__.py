a65b28dd316ea02be82453bdd414d2ece36be6c3
p-vacek <vacek@spaceknow.com>
2025-04-29 10:07:12 +0000

    correct hydra config path

--- src/GeoPixel/model/sam2/__init__.py
+++ src/GeoPixel/model/sam2/__init__.py
@@ -5,10 +5,15 @@
 # LICENSE file in the root directory of this source tree.
 
 import sys
+import os
 from hydra import initialize_config_module
 from hydra.core.global_hydra import GlobalHydra
 
+from GeoPixel.model import sam2_configs
+
 if GlobalHydra.instance().is_initialized():
     GlobalHydra.instance().clear()
-    
-initialize_config_module("model/sam2_configs", version_base="1.2")
+
+config_dir = os.path.dirname(sam2_configs.__file__)
+
+initialize_config_module(config_dir, version_base="1.2")
